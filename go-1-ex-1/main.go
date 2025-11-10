d8c53659d594d397e0d1de5a6b404800f3bf98ce
Robinasch <robinaschwanden09@gmail.com>
2025-11-10 14:59:35 +0100

    First commit

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -9,4 +9,10 @@ func main() {
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
 	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
 	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
+
+	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
+	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
+	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
+	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
+	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
 }
